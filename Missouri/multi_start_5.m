8f1c3f5463b02fc1e20f41d398a5c5c218e6b994
ibrahim Halil Aslan <asibrahimhalil@gmail.com>
2023-01-30 13:38:35 -0800

    Add files via upload

---
+++ Missouri/multi_start_5.m
@@ -0,0 +1,47 @@
+%Set the parameters 
+             %[beta       r       mu_q        i_q       gamma_q     mu_i];
+LowerBounds = [0.5         0.1     0.001      0.26      0.1      0.00014];
+UpperBounds = [2           0.4      0.02        0.3     0.14     0.001];
+
+ xstar=0.5*(LowerBounds + UpperBounds); % initial guesses for parameters
+ 
+% % % % %  Optimization Function fmincon  % % % % %
+problem=createOptimProblem('fmincon','x0',xstar, 'objective',@model_ode45_5...
+         ,'lb',LowerBounds,'ub',UpperBounds);
+
+problem.options=optimoptions(problem.options, 'MaxFunEvals',5000,'MaxIter',500);
+
+
+numstartpoints=5;
+
+ms=MultiStart('Display', 'iter'); % Define a multistar problem
+
+[x,fval, exitflag, output, manymins]=run(ms,problem,numstartpoints);
+
+global ModelParamaters5
+
+for i=1: length(manymins) 
+    ModelParamaters5(i,:)=manymins(i).X; %% model parameters
+end
+
+for i=1: length(manymins) 
+    fval(i)=manymins(i).Fval;  %% min of objective functional
+end 
+fval=fval';         
+
+for i=1: length(manymins) 
+    EF(i)=manymins(i).Exitflag;  %% must be positive 1 or 2
+end
+EF=EF';
+
+ModelParamaters5;
+
+beep on;
+beep
+beep
+beep
+beep
+beep
+beep
+
+
